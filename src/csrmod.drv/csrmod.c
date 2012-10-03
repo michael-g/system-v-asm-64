34b0420b185d2e56862288024d4efb6bf71fa5d0
Michael Guyver <michael.guyver@gmail.com>
2012-10-03 07:02:39 +0100

    Linux kernel module to set PMC read flag in CSR4
    Committing a .c file and its Makefile which together build a kernel
    driver which on load sets bit 8 of register %csr4. This permits
    application code to read the PMCs.

---
+++ src/csrmod.drv/csrmod.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int csrmod_init(void)
+{
+	long int val;
+	__asm__ __volatile__("mov %%cr4, %0" : "=r"(val));
+	val |= 0x100;           // Enable RDPMC
+	val &= ~4;              // Enable RDTSC
+	__asm__ __volatile__("mov %0, %%cr4" : : "r"(val));
+	printk(KERN_ALERT "Module csrmod loaded\n");
+	return 0;
+}
+
+static void csrmod_exit(void)
+{
+	long int val;
+	__asm__ __volatile__("mov %%cr4, %0" : "=r"(val));
+	val &= ~0x100;          // Disable RDPMC
+	__asm__ __volatile__("mov %0, %%cr4" : : "r"(val));
+	printk(KERN_ALERT "Module csrmod unloaded\n");
+}
+
+module_init(csrmod_init);
+module_exit(csrmod_exit);
+
